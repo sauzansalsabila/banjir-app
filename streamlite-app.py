1d4bec05cfe271ed85c1500d8dea0bcc398fd425
sauzansalsabila <118974171+sauzansalsabila@users.noreply.github.com>
2023-12-27 20:17:48 +0700

    Update streamlite-app.py

--- streamlite-app.py
+++ streamlite-app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 from PIL import Image
 #membaca model
-databanjir = pickle.load(open("coba.csv", "rb"))
+
 
 #judul web
 st.title("UJIAN AKHIR SISTEM INFORMASI KELAUTAN")
