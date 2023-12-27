048dd94a8edb5164858b5b9b73303b8f05fbe85d
sauzansalsabila <118974171+sauzansalsabila@users.noreply.github.com>
2023-12-27 20:17:12 +0700

    streamlite-app.py

--- streamlite-app.py
+++ streamlite-app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pickle
 from PIL import Image
+#membaca model
+databanjir = pickle.load(open("coba.csv", "rb"))
+
+#judul web
+st.title("UJIAN AKHIR SISTEM INFORMASI KELAUTAN")
 st.write('Hello Word!')
